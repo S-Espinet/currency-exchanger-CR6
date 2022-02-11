abe8b5ca4b30ef67518b5929740d29904abc7310
S. Espinet <s.lou.espinet@gmail.com>
2022-02-11 08:35:07 -0800

    add webpack config file with updated title, etc.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Weather',
+      title: 'Currency Exchanger',
       template: './src/index.html',
       inject: 'body'
     }),
