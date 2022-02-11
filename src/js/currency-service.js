dc4203579113748f0ac804d42e676011fb609c88
S. Espinet <s.lou.espinet@gmail.com>
2022-02-11 09:29:58 -0800

    add service logic to currency-exchanger

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,1 +1,17 @@
-export 
+export class CurrencyService {
+  static getCurrency(inputCurrency, inputAmount) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${inputCurrency}/${inputAmount}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        }else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
