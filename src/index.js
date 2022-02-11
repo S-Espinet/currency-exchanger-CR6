0034c7e117486d086ca6a827c4bfdbf15bed62ae
S. Espinet <s.lou.espinet@gmail.com>
2022-02-11 12:36:46 -0800

    fix capitalization

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyService from './js/currency-service.js'
+import { CurrencyService } from './js/currency-service.js';
 
-function converter(body, )
+// function converter(body, )
 
-// $(document).ready(function() {
-//   $("#exchange").click(function() {
-//     let inputAmount = parseFloat(($("#dollarAmount").val()));
-// console.log(inputAmount);
-//     let inputCurrency = $("#currencyT").val();
-// console.log(inputCurrency);
-//     let promise = CurrencyService.getExchange();
-// console.log(inputAmount);
-// console.log(inputCurrency);
-//     promise.then(function(response) {
-//       if(response) {
-//       const body = JSON.parse(response);
-//       $("#showConversion").html(`${body.conversion_result}`);
-//       }
-//     })
-//   })
-// })
+$(document).ready(function() {
+  $("#exchange").click(function() {
+    let inputAmount = parseFloat(($("#dollarAmount").val()));
+console.log(inputAmount);
+    let inputCurrency = $("#currencyT").val();
+console.log(inputCurrency);
+    let promise = CurrencyService.getExchange(inputCurrency, inputAmount);
+console.log(inputAmount);
+console.log(inputCurrency);
+    promise.then(function(response) {
+      if(response) {
+      const body = JSON.parse(response);
+      $("#showConversion").html(`${body.conversion_result}`);
+      }
+    })
+  })
+})
