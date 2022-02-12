17cc94a6ba324285669092f1cac1ffcbfffdc9e1
S. Espinet <s.lou.espinet@gmail.com>
2022-02-11 16:27:51 -0800

    add error message if invalid number is entered

--- src/index.js
+++ src/index.js
@@ -10,12 +10,20 @@ $(document).ready(function() {
   $("#exchange").click(function() {
     let inputAmount = parseFloat(($("#dollarAmount").val()));
     let inputCurrency = $("#currencyT").val();
-    let promise = CurrencyService.getExchange(inputCurrency, inputAmount);
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      $("#currencyAmount").text(`${body.conversion_result.toFixed(2)}`);
-    },function(error) {
-      $("#showErrors").text(`There was an error processing your request: ${error}`);
-    });
+    // if(inputCurrency !== "FJD" && inputCurrency !== "DKK" && inputCurrency !== "EUR" && inputCurrency !== "JPY" && inputCurrency !== "SAR") {
+    //   $("#showErrors").text("Currency not supported/currency doesn't exist. Please enter a valid currency.");
+    // }
+    if(isNaN(inputAmount) === true) {
+      $("#showErrors").text("Please enter a valid number.");
+    }
+    else {
+      let promise = CurrencyService.getExchange(inputCurrency, inputAmount);
+      promise.then(function(response) {
+        let body = JSON.parse(response);
+        $("#currencyAmount").text(`${body.conversion_result.toFixed(2)}`);
+      },function(error) {
+        $("#showErrors").text(`There was an error processing your request: ${error}`);
+      });
+    }
   });
 });
