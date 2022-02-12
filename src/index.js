151cf85951b0fa679dbc869940eb49d7960d7a41
S. Espinet <s.lou.espinet@gmail.com>
2022-02-12 12:31:53 -0800

    update error message to include user error in currency

--- src/index.js
+++ src/index.js
@@ -13,19 +13,31 @@ $(document).ready(function() {
     if(isNaN(inputAmount) === true) {
       $("#showErrors").text("Please enter a valid number.");
     }
-    else if(inputCurrency !== "FJD" && inputCurrency !== "DKK" && inputCurrency !== "EUR" && inputCurrency !== "JPY" && inputCurrency !== "SAR") {
-      $("#showErrors").text("Currency not supported/currency doesn't exist. Please enter a valid currency.");
-    }
     else {
       let promise = CurrencyService.getExchange(inputCurrency, inputAmount);
       promise.then(function(response) {
-
         let body = JSON.parse(response);
         $("#showConversion").show();
-        $("#currencyAmount").text(`${body.conversion_result.toFixed(2)}`);  
-      },function(error) {
-        $("#showErrors").text(`There was an error processing your request: ${error}`);
+        $("#currencyAmount").text(`${body.conversion_result.toFixed(2)}`);
+        $("#showUserErrors").hide();
+        $("#showErrors").hide();
+      }, function(error) {
+        let body = JSON.parse(error);
+        if (body["error-type"] === "malformed-request") {
+          $("#showUserErrors").text(`Currency not supported/currency does not exist: ${error}`);
+          $("#showConversion").hide();
+        }
+        else {
+          $("#showErrors").text(`There was an error processing your request: ${error}`);
+          $("#showConversion").hide();
+        }
       });
     }
   });
 });
+
+
+
+          // if(inputCurrency !== "FJD" && inputCurrency !== "DKK" && inputCurrency !== "EUR" && inputCurrency !== "JPY" && inputCurrency !== "SAR") {
+          //   $("#showErrors").text("Currency not supported/currency doesn't exist. Please enter a valid currency.");
+          // }
